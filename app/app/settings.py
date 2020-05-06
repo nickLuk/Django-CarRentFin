e225bfcc7ba84de9aeb3f497adcb912b41cf966a
nickLuk <lukyanik@ukr.net>
2020-05-06 11:19:58 +0300

    two

--- app/app/settings.py
+++ app/app/settings.py
@@ -139,8 +139,8 @@ MESSAGE_TAGS = {
     
 }
 
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = "lukyanik.iawp@gmail.com"
-EMAIL_HOST_PASSWORD = ''
-EMAIL_USE_TLS = True
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = "lukyanik.iawp@gmail.com"
+# EMAIL_HOST_PASSWORD = ''
+# EMAIL_USE_TLS = True
